67daed1a764883152e475225a71705f1ef4e6171
hnix <hnix.au@gmail.com>
2016-10-16 18:27:42 -0500

    SMPT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.gmail.com",
+    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "GMAIL_USERNAME",
+    :password  => "GMAIL_PASSWORD", # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'gmail.com', # your domain to identify your server when connecting
+  }
 end
