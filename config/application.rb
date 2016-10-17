94b51965fabc16f7f1c0a50b5bc7a69bb60a6f9b
hnix <hnix.au@gmail.com>
2016-10-16 19:19:18 -0500

    Serve static assets

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Aerialhaus
   class Application < Rails::Application
-
+    config.serve_static_assets = true
+    
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -20,7 +21,7 @@ module Aerialhaus
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
