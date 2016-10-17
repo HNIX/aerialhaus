5d4a59afd089583b64f86bfa62c61240987f7635
hnix <hnix.au@gmail.com>
2016-10-16 22:36:31 -0500

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   config.serve_static_assets = false
 
