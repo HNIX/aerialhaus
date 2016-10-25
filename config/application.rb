e2c61a0d90003a38002e6ef057c4d96347a9b848
hnix <hnix.au@gmail.com>
2016-10-25 13:06:56 -0500

    fix fonts

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Aerialhaus
   class Application < Rails::Application
-    
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
@@ -20,7 +20,8 @@ module Aerialhaus
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
-
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
