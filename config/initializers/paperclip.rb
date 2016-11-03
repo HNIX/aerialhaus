938be060f30fc94b71635aec441d2f8364f0067a
hnix <hnix.au@gmail.com>
2016-11-03 15:31:53 -0500

    s3 config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,6 +5,7 @@ if ENV['AWS_ACCESS_KEY_ID']
       :provider => "AWS",
       :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :region                 => ENV['AWS_REGION']
     },
     :fog_directory => ENV["S3_BUCKET_NAME"]
   )
